834bc12484eb6c813a183659080f00e159d90a7c
Vaishnavi Ladda <105637457+vaishnavi2833@users.noreply.github.com>
2026-05-25 21:15:36 +0530

    new code outer files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/fetch_circulars.php': {
+        target: 'https://anilvishwakarma.co.in/kb/circulars',
+        changeOrigin: true,
+        secure: false,   // accept self-signed / untrusted certs
+        rewrite: path => path,
+      },
+    },
+  },
 })
